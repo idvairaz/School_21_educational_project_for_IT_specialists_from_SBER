4436c2fd145f06f2e59eb8e9a3fbaea72cbd6913
ira <gymn-1razumova@yandex.ru>
2024-04-30 22:48:17 +0300

    fix ex9

--- sql/SQL_beginner.Day07-1/src/ex09/day07_ex09.sql
+++ sql/SQL_beginner.Day07-1/src/ex09/day07_ex09.sql
@@ -1,10 +1,7 @@
 SELECT address,
        round(max(age)::NUMERIC - (min(age)::NUMERIC / max(age)::NUMERIC), 2) AS formula,
        round(avg(age), 2)                                                    AS average,
-       CASE
-           WHEN MAX(age) - (MIN(age) / MAX(age)) > AVG(age) THEN 'True'
-           ELSE 'False'
-           END                                                               AS comparison
+       MAX(age) - (MIN(age) / MAX(age)) > AVG(age)                           AS comparison
 FROM person
 GROUP BY address
 ORDER BY address;
